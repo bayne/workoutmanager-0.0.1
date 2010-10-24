9ca12a938c86012fb335653ec9c38487624a4a7d
Brian Payne <bwpayne@gmail.com>
2010-10-23 20:01:35 -0700

    Added migration

--- _lib/db/workoutmanager.sql
+++ _lib/db/workoutmanager.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 22, 2010 at 05:10 PM
+-- Generation Time: Oct 23, 2010 at 08:01 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -32,17 +32,32 @@ CREATE TABLE IF NOT EXISTS `exercises` (
   `rep_max` int(11) NOT NULL,
   `weight_min` int(11) NOT NULL,
   `weight_max` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `name` text NOT NULL,
   `number_of_sets` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `id` (`id`,`workout_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `exercises`
 --
 
 INSERT INTO `exercises` (`id`, `workout_id`, `rep_min`, `rep_max`, `weight_min`, `weight_max`, `name`, `number_of_sets`) VALUES
-(0, -1, 1, 1, 1, 1, 1, 1);
+(1, 18, 0, 0, 0, 0, 'test', 0),
+(0, 18, 0, 0, 0, 0, 'test221', 0),
+(9, 17, 0, 0, 0, 0, 'test', 0),
+(8, 17, 0, 0, 0, 0, 'test2', 0),
+(0, 19, 0, 0, 0, 0, 'test2', 0),
+(1, 19, 0, 0, 0, 0, 'test', 0),
+(7, 17, 0, 0, 0, 0, 'test', 0),
+(6, 17, 0, 0, 0, 0, 'test2', 0),
+(5, 17, 0, 0, 0, 0, 'test', 0),
+(4, 17, 0, 0, 0, 0, 'test2', 0),
+(3, 17, 0, 0, 0, 0, 'test2', 0),
+(2, 17, 0, 0, 0, 0, 'test', 0),
+(1, 17, 0, 0, 0, 0, 'test2', 0),
+(0, 17, 0, 0, 0, 0, 'test', 0),
+(10, 17, 0, 0, 0, 0, 'test2', 0),
+(11, 17, 0, 0, 0, 0, 'test', 0);
 
 -- --------------------------------------------------------
 
@@ -55,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` text NOT NULL,
   `password` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `users`
@@ -67,7 +82,8 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (3, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
 (4, '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709'),
 (5, 'new', 'c2a6b03f190dfb2b4aa91f8af8d477a9bc3401dc'),
-(6, 'new', 'c2a6b03f190dfb2b4aa91f8af8d477a9bc3401dc');
+(6, 'new', 'c2a6b03f190dfb2b4aa91f8af8d477a9bc3401dc'),
+(7, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 
 -- --------------------------------------------------------
 
@@ -76,18 +92,20 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `workouts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `workout_name` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
 -- Dumping data for table `workouts`
 --
 
 INSERT INTO `workouts` (`id`, `user_id`, `workout_name`) VALUES
-(0, 5, '');
+(19, 3, ''),
+(18, 3, ''),
+(17, 3, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
